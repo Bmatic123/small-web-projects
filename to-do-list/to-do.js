ea0615f429da507130941bc0d13b6dafd812bb66
Vighnesh Shenoy <vighneshq@gmail.com>
2018-05-07 00:36:09 +0530

    All the functionality and styling.

--- to-do-list/to-do.js
+++ to-do-list/to-do.js
@@ -1,5 +1,23 @@
 
-
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("done");
+});
+
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(250, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input").keypress(function(event){
+	if(event.which === 13){
+		var text = $(this).val();
+		$("ul").append("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span>"  + text + " </li>")
+		$(this).val("");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input").fadeToggle();
 });
